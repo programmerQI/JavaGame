0d0ac6cc0eb4a1298826dca78d665c9ff824944c
QIWANG <qiwa347@colorado.edu>
2020-01-08 16:34:29 -0700

    updated

--- SpaceInvaders/src/com/cn/game/Invaders.java
+++ SpaceInvaders/src/com/cn/game/Invaders.java
@@ -1,16 +1,81 @@
 package com.cn.game;
 
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+
 public class Invaders {
+	
+	private static final String DIR_IMG = "images/invader.png";
+	private static final int DWIDTH_IMG = 32;
+	private static final int DHEIGHT_IMG = 32;
 	
 	class Invader {
 		
 		boolean alive;
+		int x;
+		int y;
 		
+		public Invader(boolean alive, int x, int y) {
+			this.alive = alive;
+			this.x = x;
+			this.y = y;			
+		}
 		
 	}
 	
+	private BufferedImage img;
+	// The source image width and height
+	private int sw_img;
+	private int sh_img;
+	
+	private Invader[][] ins;
+	// The coordinate of the invaders
+	private int x, y;
 	private int rows;
-	private int colums;
-	private int speed;
+	private int columns;
+	// The space between each invader
+	private int space;
+	
+	public Invaders(int x, int y, int rows, int columns, int space) {
+		
+		try {
+			img = ImageIO.read(new File(DIR_IMG));
+			sw_img = img.getWidth();
+			sh_img = img.getHeight();
+		} catch (IOException e) {
+			System.out.println("Can not load Invader image!");
+			e.printStackTrace();
+		}
+		
+		this.x = x;
+		this.y = y;
+		this.rows = rows;
+		this.columns = columns;
+		this.space = space;
+		for(int i = 0; i < rows; i ++) {
+			for(int j = 0; j < columns; j ++) {
+				int ix = x + space * j + DWIDTH_IMG * j;
+				int iy = y + space * i + DHEIGHT_IMG * i;
+				ins[i][j]  = new Invader(true, ix, iy); 
+			}
+		}
+	}
+	
+	void draw(Graphics g) {
+		for(int i = 0; i < rows; i ++) {
+			for(int j = 0; j < columns; j ++) {
+				Invader in = ins[i][j];
+				int dx1 = in.x;
+				int dy1 = in.y;
+				int dx2 = in.x + DWIDTH_IMG;
+				int dy2 = in.y + DHEIGHT_IMG;
+				g.drawImage(img, dx1, dy1, dx2, dy2, 0, 0, sw_img, sh_img, null);
+			}
+		}
+	}
 
 }
--- SpaceInvaders/src/com/cn/game/Main.java
+++ SpaceInvaders/src/com/cn/game/Main.java
@@ -1,12 +1,32 @@
 package com.cn.game;
 
+import java.awt.Graphics;
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JPanel;
 
 public class Main {
 
 	public static void main(String[] args) {
 		
-		Window window = new Window(400, 400, new JPanel());
+		
+		
+		Window window = new Window(400, 400, new JPanel() {
+			@Override
+			public void paint(Graphics g) {
+				super.paint(g);
+				Invaders invaders = new Invaders(0, 0, 10, 10, 5);
+				invaders.draw(g);
+			}
+		});
+		
+		
+		
 		window.setVisible(true);
 
 	}
